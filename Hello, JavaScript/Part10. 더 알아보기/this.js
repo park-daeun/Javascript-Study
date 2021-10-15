30f5d7661833c2263ee2e4c1dd0cd4fe8726e5bb
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-10-15 19:12:27 +0900

    Hello, JavaScript 예제

---
+++ Hello, JavaScript/Part10. 더 알아보기/this.js
@@ -0,0 +1,19 @@
+function f(){
+    console.log(this);
+    console.log("f is called");
+}
+
+function setName(name){
+    this.name = name;
+}
+
+var o = {name:"object", method:f, setName:setName};
+var o2 = {name:"", setName:setName};
+
+f();
+o.method();
+
+o.setName("object1");
+o2.setName("object2");
+
+console.log(o, o2);
